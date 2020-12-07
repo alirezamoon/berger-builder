568a8dd01baba765138fa1b39c33bf27da60bb09
Alireza <real7alireza@gmail.com>
2020-12-08 01:11:27 +0330

    add Layout and BurgerBuild Components

---
+++ src/Components/Layout/Layout.js
@@ -0,0 +1,10 @@
+import Aux from "./../../hoc/Auxx";
+import classes from "./Layout";
+const layout = (props) => (
+  <Aux>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main className={classes.Content}>{props.children}</main>
+  </Aux>
+);
+
+export default layout;
