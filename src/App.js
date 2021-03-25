cf85432f4482a845a0648b8b6549965cfd2f7b44
real7alireza <real7alireza@gmail.com>
2021-03-25 19:09:18 +0430

    adding an auth form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./Containers/BurgerBuilder/BurgerBuilder";
 import Checkout from './Containers/Checkout/Checkout'
 import Orders from './Containers/Orders/Orders'
+import Auth from './Containers/Auth/Auth'
 
 import { Route, Switch } from 'react-router-dom'
 
@@ -13,6 +14,7 @@ function App() {
         <Switch>
           <Route path='/checkout' component={Checkout} />
           <Route path='/orders' component={Orders} />
+          <Route path='/auth' component={Auth} />
           <Route path='/' exact component={BurgerBuilder} />
         </Switch>
       </Layout>
---
+++ src/Containers/Auth/Auth.js
@@ -0,0 +1,115 @@
+import React, { useState } from 'react'
+import Input from './../../Components/UI/Input/Input'
+import Button from './../../Components/UI/Button/Button'
+import classes from './Auth.module.css'
+
+
+const Auth = () => {
+    const [controls, setControls] = useState(
+        {
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Mail Address'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    isEmail: true
+                },
+                valid: false,
+                touched: false
+            },
+            password: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'password',
+                    placeholder: 'Password'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 6
+                },
+                valid: false,
+                touched: false
+            }
+        }
+    )
+
+    const inputChangedHandler = (event, controlName) => {
+        const updatedControls = {
+            ...controls,
+            [controlName]: {
+                ...controls[controlName],
+                value: event.target.value,
+                valid: checkValidity(event.target.value, controls[controlName].validation),
+                touched: true
+            }
+        }
+        setControls(updatedControls)
+    }
+
+    const checkValidity = (value, rules) => {
+        let isValid = true;
+
+        if (!rules) {
+            return true;
+        }
+
+        if (rules.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
+
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid
+        }
+
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid
+        }
+
+        if (rules.isEmail) {
+            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+            isValid = pattern.test(value) && isValid
+        }
+
+        if (rules.isNumeric) {
+            const pattern = /^\d+$/;
+            isValid = pattern.test(value) && isValid
+        }
+
+        return isValid;
+    }
+
+    const fromElementArray = []
+    for (let key in controls) {
+        fromElementArray.push({
+            id: key,
+            config: controls[key]
+        })
+    }
+    const form = fromElementArray.map(formElement => (
+        <Input
+            key={formElement.id}
+            elementType={formElement.config.elementType}
+            elementConfig={formElement.config.elementConfig}
+            value={formElement.config.value}
+            invalid={!formElement.config.valid}
+            shouldValidate={formElement.config.validation}
+            toched={formElement.config.touched}
+            changed={(event) => inputChangedHandler(event, formElement.id)}
+        />
+    ))
+    return (
+        <div className={classes.Auth}>
+            <form>
+                {form}
+                <Button btnType='Success'>SUBMIT</Button>
+            </form>
+        </div>
+    )
+}
+
+export default Auth
